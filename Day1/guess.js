60a6fe03486fe85cc4e14b4657b2471505ce38bb
Chun-Lin Wu <wulin40063@gmail.com>
2016-08-15 02:42:12 +0800

    fix some bugs

--- Day1/guess.js
+++ Day1/guess.js
@@ -1,22 +1,31 @@
 var answer = prompt("Please enter your answer");
-
+// answer = Big(answer);
 var count = 1
 
 function guess() {
     var input = document.getElementById("input_number").value;
+    // input = Big(input);
+    console.log("answer:" + answer);
+    console.log("input:" + input);
 
-    
-
-    if (input >= 0 && input <= 100 && input !="") {
+    if (input >= 0 && input < 101 && input != "") {
         if (input == answer) {
             document.getElementById("result").innerHTML = "Congratulations! The answer is " + answer;
             document.getElementById("count").innerHTML = "You guessed " + count + " times";
             document.getElementById("result").style.color = "blue";
             document.getElementById("count").style.color = "red";
-        } else if (input < answer) {
-count++;
+        }
+
+        console.log("answer1:" + answer);
+        console.log("input1:" + input);
+        if (input < answer) {
+            count++;
             document.getElementById("result").innerHTML = "Guess bigger number!";
-        } else {
+        }
+
+        console.log("answer2:" + answer);
+        console.log("input2:" + input);
+        if (input > answer) {
             count++;
             document.getElementById("result").innerHTML = "Guess smaller number!";
         }
